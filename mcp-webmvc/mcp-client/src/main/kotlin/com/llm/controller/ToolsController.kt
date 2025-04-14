181d8e3be5a037e40c5b04f7d299593d246e79b0
Dilip.Sundarraj <dilip.sundarraj@gmail.com>
2025-04-14 05:52:47 -0500

    added the code for insomnium

--- mcp-webmvc/mcp-client/src/main/kotlin/com/llm/controller/ToolsController.kt
+++ mcp-webmvc/mcp-client/src/main/kotlin/com/llm/controller/ToolsController.kt
@@ -4,8 +4,15 @@ import io.modelcontextprotocol.client.McpSyncClient
 import io.modelcontextprotocol.spec.McpSchema
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.ai.mcp.SyncMcpToolCallback
+import org.springframework.ai.mcp.SyncMcpToolCallbackProvider
+import org.springframework.ai.tool.ToolCallback
+import org.springframework.ai.tool.ToolCallbackProvider
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+
 
 @RestController
 class ToolsController(private val mcpClients: List<McpSyncClient>) {
@@ -21,5 +28,42 @@ class ToolsController(private val mcpClients: List<McpSyncClient>) {
         }
 
         return mcpClients.map { it.listTools() }
+    }
+
+    @GetMapping("/v1/tools/invoke")
+    fun invokeTools(
+        @RequestParam("tool_name") toolName: String,
+        @RequestParam("tool_input_key") toolInputKey: String,
+        @RequestParam("tool_input_value") toolInputValue: String,
+        @RequestParam("mcp_server") mcpServer: String,
+
+    ): Any {
+
+        val mcpClient = mcpClients.firstOrNull { it.clientInfo.name.contains(mcpServer, true) }
+
+        if (mcpClient == null)
+            return ResponseEntity.badRequest().body("MCP server not found: $mcpServer")
+
+        log.info("clientInfo : {} , serverCapabilities : {}  ", mcpClient.clientInfo, mcpClient.serverCapabilities)
+
+        val mcpTool = mcpClient.listTools()
+            .tools.first { it.name == toolName }
+
+        log.info("mcpTool : {} ", mcpTool)
+        return  if(mcpTool == null) {
+            log.error("Tool not found: $toolName")
+            "Tool not found: $toolName"
+        }else{
+            val callback: ToolCallback = SyncMcpToolCallback(mcpClient, mcpTool)
+            val inputJson =
+                """
+                {
+                    "$toolInputKey": "$toolInputValue"
+                }""".trimIndent()
+            log.info("inputJson : {} ", inputJson)
+             val toolCallResult = callback.call(inputJson)
+            log.info("Tool call result: $toolCallResult")
+            toolCallResult
+        }
     }
 }
